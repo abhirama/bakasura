f413775e7d033729d2f806edd7ef339cba9861c6
abhirama <abhyrama@gmail.com>
2012-08-02 19:16:58 +0530

    Commenting.

--- src/com/abhirama/gameengine/tests/stresstest/CustomGameServerHandler.java
+++ src/com/abhirama/gameengine/tests/stresstest/CustomGameServerHandler.java
@@ -10,11 +10,18 @@ import com.abhirama.http.GameServerHandler;
 import com.abhirama.utils.Util;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class CustomGameServerHandler extends GameServerHandler {
 
   @Override
   public Map gameLogic(Map data) {
+    //Simulate a memcache fetch
+    try {
+      TimeUnit.MILLISECONDS.sleep(2);
+    } catch (InterruptedException e) {
+      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+    }
     Room room = Room.getRoom(Util.getRandomInt(1, 1000));
 
     HitEvent hitEvent = new HitEvent();
@@ -23,6 +30,12 @@ public class CustomGameServerHandler extends GameServerHandler {
     room.setRoomEvent(hitRoomEvent);
 
     room.executeRoomEvent(hitEvent);
+    //Simulate a memcache fetch
+    try {
+      TimeUnit.MILLISECONDS.sleep(2);
+    } catch (InterruptedException e) {
+      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+    }
 
     this.addToOp("I am inside custom game server handler" + this.requestParameters.toString());
 
